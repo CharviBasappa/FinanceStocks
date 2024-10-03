2efecd798cd2c6c7a4c8c0f2742e92f47299826c
Charvi <charvibasappa98@gmail.com>
2024-10-03 11:33:47 -0500

    Using Props the card is designed dynamically

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./Card.css";
 
-type Props = {};
+interface Props {
+    companyName: string;
+    ticker: string;
+    price: number;
+}
 
-const Card = (props: Props) => {
+const Card = ({ companyName, ticker, price}: Props) => {
   return (
     <div className="card">
       <img src="Images/Image1.jpg" alt="Image"></img>
       <div className="details">
-        <h2>Apple</h2>
-        <p>$100</p>
+              <h2>{companyName} ({ticker})</h2>
+              <p>${price}</p>
       </div>
       <p className="info">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, veniam?
